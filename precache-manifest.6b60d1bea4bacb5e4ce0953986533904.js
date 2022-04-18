141653358758c94f3f9ed9ec1cf1306510b6d139
heathenthrone48 <ashinsrajgad@gmail.com>
2022-04-18 19:45:46 +0530

    Updates

---
+++ precache-manifest.6b60d1bea4bacb5e4ce0953986533904.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "928aa5adfe02574b76cc",
+    "url": "/expense-tracker/static/css/main.13d8b0e5.chunk.css"
+  },
+  {
+    "revision": "928aa5adfe02574b76cc",
+    "url": "/expense-tracker/static/js/main.928aa5ad.chunk.js"
+  },
+  {
+    "revision": "f71a4d243061bb81d938",
+    "url": "/expense-tracker/static/js/1.f71a4d24.chunk.js"
+  },
+  {
+    "revision": "2c594b5b6bfedf8ec159",
+    "url": "/expense-tracker/static/js/runtime~main.2c594b5b.js"
+  },
+  {
+    "revision": "8bfd939db52a1bd8205c3bb80c230d71",
+    "url": "/expense-tracker/index.html"
+  }
+];
